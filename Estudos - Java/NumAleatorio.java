e91d1aee1562dcedd0c04addfb6e2857a263a46f
Willian Santos <49081567+ykandakota@users.noreply.github.com>
2020-11-25 10:33:38 -0300

    exercicio - criando numeros aleatorios com a classe Random

--- Estudos - Java/NumAleatorio.java
+++ Estudos - Java/NumAleatorio.java
@@ -1,4 +1,15 @@
+/*
+Estudos de  Java  
+Usando notepad e JDK 15.0.1
 
+livro de referencia: 
+Java: como programar 8ª edição 
+Deitel,Paul; Deitel Harvey M.
+ 
+data: 25/NOV/2020
+autor: Willian Santos 
+
+*/
 
 	
 import java.util.Random;	//importa a classe random do pacote java util 
